12ebbbc8c938dfca4ebb7dd9016e5b6231d01f77
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-09-20 17:29:23 +0300

    add reset styles

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -22,6 +22,48 @@ body {
   -moz-osx-font-smoothing: grayscale;
   }
 
+  *,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+ul,
+ol {
+  padding: 0;
+  margin: 0;
+}
+ul {
+  list-style: none;
+}
+li {
+  list-style: none;
+}
+a {
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+
+p {
+  padding: 0;
+  margin: 0;
+}
+
   //Fonts
   @font-face {
   font-family: 'FixelDisplay';
