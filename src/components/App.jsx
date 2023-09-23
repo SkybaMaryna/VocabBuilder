33ef0a0f95e3739b4f202c49f19642deeeffbdb4
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-09-23 12:53:49 +0300

    add hoc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from 'pages/LoginPage/LoginPage';
+import RegisterPage from 'pages/RegisterPage/RegisterPage';
+import { PublicRoute } from 'hoc';
+
 export const App = () => {
   return (
-    <>Hello</>
+    <Routes>
+      <Route
+        path="login"
+        element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="register"
+        element={
+          <PublicRoute>
+            <RegisterPage />
+          </PublicRoute>
+        }
+      />
+      <Route path='*' />
+    </Routes>
   );
 };
